14bf7708bc0b249cfd5f8005cec01ecb55199a6e
OiPKL <130027416+dhkswls1998@users.noreply.github.com>
2023-11-06 22:30:28 +0900

    https

--- app/main.py
+++ app/main.py
@@ -11,7 +11,9 @@ app = FastAPI()
 
 # CORS 설정 : local test -> 주석처리 변경
 # origins = ["http://localhost:8080"]
-origins = ["http://59.5.235.142:8080"]
+# origins = ["https://localhost:8080"]
+# origins = ["http://59.5.235.142:8080"]
+origins = ["https://59.5.235.142:8080"]
 
 app.add_middleware(
     CORSMiddleware,
@@ -33,6 +35,21 @@ app.include_router(router_user.router)
 # app.include_router(router_vegetable.router)
 # app.include_router(router_garden.router)
 
-if __name__ == "__main__":
+# if __name__ == "__main__":
+#     uvicorn.run(app, host="0.0.0.0", port=8080)
+
+if __name__ == '__main__':
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8080)
+    from subprocess import Popen
+
+    Popen(['python', '-m', 'https_redirect'])
+    uvicorn.run(
+        'main:app', host="0.0.0.0", port=8080,
+        reload=True, reload_dirs=['html_files'],
+        ssl_keyfile='C:/Users/dhksw/Documents/work/key.pem',
+        ssl_certfile='C:/Users/dhksw/Documents/work/cert.pem'
+    )
+    
+
+# uvicorn app.main:app --host 0.0.0.0 --port 8080 --reload
+# uvicorn app.main:app --host 0.0.0.0 --port 8080 --reload --ssl-keyfile=./key.pem --ssl-certfile=./cert.pem
