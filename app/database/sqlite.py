e55d6b632943f40ab37d38e8c83b94ea8b294403
dhkswls1998 <dhkswls1998@naver.com>
2023-11-06 23:04:03 +0900

    https

--- app/database/sqlite.py
+++ app/database/sqlite.py
@@ -1,16 +1,14 @@
 # 데이터베이스 연결 엔진
 from sqlalchemy import create_engine
-# 기본클래스 Base
-from sqlalchemy.ext.declarative import declarative_base
 # 세션생성기
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import declarative_base, sessionmaker
 # 세션클래스
 from sqlalchemy.orm import Session
 
 # 현재 디렉토리에 test.db 생성
-DATABASE_URL = "sqlite:///./test.db"
+DATABASE_URL = "sqlite:///./test.sqlite"
 
-# 데이터베이스 연결 엔진 생성하고 test.db 에 연결
+# 데이터베이스 연결 엔진 생성하고 test.sqlite 에 연결
 engine = create_engine(DATABASE_URL)
 
 # 세션생성기 정의 : 세션의 자동 커밋 및 자동 플러시 비활성화
