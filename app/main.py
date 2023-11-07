bf33b8595d8fa8cb15ccc89b8f4cb5de10b39bb5
dhkswls1998 <dhkswls1998@naver.com>
2023-11-07 14:35:25 +0900

    test

--- app/main.py
+++ app/main.py
@@ -10,10 +10,10 @@ from app.routers import router_garden
 app = FastAPI()
 
 # CORS 설정 : local test -> 주석처리 변경
-# origins = ["http://localhost:8080"]
+origins = ["http://localhost:8080"]
 # origins = ["https://localhost:8080"]
 # origins = ["http://59.5.235.142:8080"]
-origins = ["https://59.5.235.142:8080"]
+# origins = ["https://59.5.235.142:8080"]
 
 app.add_middleware(
     CORSMiddleware,
@@ -35,20 +35,21 @@ app.include_router(router_user.router)
 # app.include_router(router_vegetable.router)
 # app.include_router(router_garden.router)
 
-# if __name__ == "__main__":
-#     uvicorn.run(app, host="0.0.0.0", port=8080)
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     import uvicorn
-    from subprocess import Popen
+    uvicorn.run(app, host="0.0.0.0", port=8080)
 
-    Popen(['python', '-m', 'https_redirect'])
-    uvicorn.run(
-        'main:app', host="0.0.0.0", port=8080,
-        reload=True, reload_dirs=['html_files'],
-        ssl_keyfile='C:/Users/dhksw/Documents/work/key.pem',
-        ssl_certfile='C:/Users/dhksw/Documents/work/cert.pem'
-    )
+# if __name__ == '__main__':
+#     import uvicorn
+#     from subprocess import Popen
+
+#     Popen(['python', '-m', 'https_redirect'])
+#     uvicorn.run(
+#         'main:app', host="0.0.0.0", port=8080,
+#         reload=True, reload_dirs=['html_files'],
+#         ssl_keyfile='C:/Users/dhksw/Documents/work/key.pem',
+#         ssl_certfile='C:/Users/dhksw/Documents/work/cert.pem'
+#     )
     
 
 
